f470e83c68b28a2a4b0de9188f90762a836f28ad
Timothy King <timothy.king@networkforgood.com>
2020-05-07 08:32:45 -0400

    Moves callbacks to settings.rb

--- lib/nfg/capistrano/config/settings.rb
+++ lib/nfg/capistrano/config/settings.rb
@@ -35,3 +35,39 @@ set :whenever_environment, ->{ "#{fetch(:stage)}" }
 # capistrano-measure
 set :alert_threshold, 10              # default 60 sec
 set :warning_threshold, 5             # default 30 sec
+
+before 'deploy:check:linked_files', 'config:check:upload_setup_files'
+before 'config:check:upload_setup_files', 'config:check:setup_files_exists_local'
+after 'config:check:upload_setup_files', 'config:check:check_apikeys_download_from_s3'
+
+if ENV['PACKER'] == 'y' || fetch(:stage) == :development
+  before 'rvm:hook', 'aws:deploy:set_app_instances_to_local'
+
+elsif fetch(:app_instances) or fetch(:worker_instances)
+  print "-> Cron jobs will NOT be installed during manual deployments.\n->\n"
+
+  if fetch(:app_instances)
+    fetch(:app_instances).each do |instance|
+      print "-> Deploying to app instance #{instance}\n"
+      server instance, user: fetch(:app_user), roles: %w(web app)
+    end
+  end
+
+  if fetch(:worker_instances)
+    fetch(:worker_instances).each do |instance|
+      print "-> Deploying to worker instance #{instance}\n"
+      server instance, user: fetch(:app_user), roles: %w(resque_worker resque_scheduler)
+    end
+  end
+else
+  before 'deploy:migrate', 'migrations:check'
+  before 'rvm:hook', 'aws:deploy:fetch_running_instances'
+  after 'aws:deploy:fetch_running_instances', 'aws:deploy:confirm_running_instances'
+  after 'aws:deploy:confirm_running_instances', 'aws:deploy:set_app_instances_to_live'
+  after 'aws:deploy:set_app_instances_to_live', 'aws:deploy:print_servers'
+end
+
+if defined?(CapistranoResque)
+  after "deploy:restart", "resque:restart"
+  after "resque:restart", "resque:scheduler:restart"
+end
