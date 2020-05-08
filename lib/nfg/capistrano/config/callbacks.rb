0fe9ae540015372269ced8d1caf1955655b6dd1a
Timothy King <timothy.king@networkforgood.com>
2020-05-08 08:07:06 -0400

    Fixes indentation

--- lib/nfg/capistrano/config/callbacks.rb
+++ lib/nfg/capistrano/config/callbacks.rb
@@ -1,5 +1,5 @@
 if ENV['PACKER'] == 'y' || fetch(:stage) == :development
-  before 'rvm:hook', 'aws:deploy:set_app_instances_to_local'
+	before 'rvm:hook', 'aws:deploy:set_app_instances_to_local'
 else
 	before 'rvm:hook', 'aws:deploy:fetch_running_instances'
 end
