8e546e75bbb824f09d299d1668a4f8f9bfc2dbca
Timothy King <timothy.king@networkforgood.com>
2020-08-06 13:04:20 -0400

    Adds sidekiq cap tasks

--- lib/nfg/capistrano/aws.rb
+++ lib/nfg/capistrano/aws.rb
@@ -1,6 +1,6 @@
 require 'nfg/capistrano/config/settings'
 
-%w(aws.rake config.rake migrations.rake).each do |file|
+%w(sidekiq.rake aws.rake config.rake migrations.rake).each do |file|
   load File.expand_path("../tasks/#{file}", __FILE__)
 end
 
