73dc0df3db0257b9e7f485b66719500003a76106
Timothy King <timothy.king@networkforgood.com>
2023-04-21 11:03:07 -0400

    Adds task to remove bundler's git cache dir

--- lib/nfg/capistrano/config/callbacks.rb
+++ lib/nfg/capistrano/config/callbacks.rb
@@ -1,6 +1,7 @@
 if ENV['PACKER'] == 'y' || fetch(:stage) == :development
-	before 'rvm:hook', 'aws:deploy:set_app_instances_to_local'
+  before 'rvm:hook', 'aws:deploy:set_app_instances_to_local'
 else
-	before 'rvm:hook', 'aws:deploy:fetch_running_instances'
+  before 'rvm:hook', 'aws:deploy:remove_git_cache_dir'
+  before 'rvm:hook', 'aws:deploy:fetch_running_instances'
 end
 
